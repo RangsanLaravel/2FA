76c8325ebbcd6b00c8b6bf146f6a4dc182ec32c4
Rangsan Chipphang <rangsan.chi@bla.co.th>
2021-09-14 20:14:04 +0700

    2FA Frirst Commit

---
+++ Models/FactorUser.cs
@@ -0,0 +1,17 @@
+﻿using Google.Authenticator;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2FA.Models
+{
+    public class FactorUser
+    {
+        [Required]
+        public string UserInputCode { get; set; }
+        public SetupCode setupcode { get; set; }
+        public bool AuthenSuccess { get; set; }
+    }
+}
